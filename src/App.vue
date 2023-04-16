2ddf965a067b3f85a7deadbfc5ef2bc2721eb38a
5Zgs1 <3460755469@qq.com>
2023-04-16 23:33:11 +0800

    [新增功能](master): 引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
 <h1>校内论坛</h1>
 <!--  路由的出口-->
+  <i class="iconfont icon-wode">我的</i>
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success">成功按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+
   <router-view/>
 </template>
 <script>
