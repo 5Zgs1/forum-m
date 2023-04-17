113fa8be12e061e1fb8d67590d175f4b709057b4
5Zgs1 <3460755469@qq.com>
2023-04-17 11:36:05 +0800

    [新增功能](master): 根据机型选择要的配置
    mos 之类的在原来的vant就已经配置好了，不需要再配置

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,11 +1,19 @@
 // postcss.config.js
+// postcss配置文件
 module.exports = {
   plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'ios >= 8']
-    },
+    // autoprefixer: {
+    //   browsers: ['Android >= 4.0', 'ios >= 8']
+    // },
+    // 把px转为rem
     'postcss-pxtorem': {
-      rootValue: 37.5,
+      // rootValue设置为设计稿的十分之一
+      rootValue ({ file }) {
+        return file.indexOf('vant') !== -1 ? 37.5 : 75
+      },
+      // rootValue: 37.5,
+      // 配置要转换的css属性
+      //* 表示所有
       propList: ['*']
     }
   }
