538afefdc40f4d28e082b62dac8728414ad050f2
5Zgs1 <3460755469@qq.com>
2023-04-24 23:33:44 +0800

    [新增功能](master): 展示列表内容
    关注使用插槽（视频的写法已经被弃用了）

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,7 +1,32 @@
 <template>
   <div >
-    <van-cell title="danyuge">
-      <template v-slot:title>{{ article.title }}</template>
+    <van-cell title="danyuge" class="article-item">
+      <template v-slot:title >{{ article.title }}
+        <slot class="title" >
+          <div class="cover-wrap" v-if="article.cover.type ===3">
+            <div class="cover-item"
+                 v-for="(img, index) in article.cover.images"
+            :key="index">
+              <van-image
+                width="100"
+                height="100"
+                :src="img"
+              />
+            </div>
+          </div>
+          <div>
+            <span>{{article.aut_name}}</span>
+            <span>{{article.comm_count}}条评论</span>
+            <span>{{article.pubdate}}</span>
+          </div>
+        </slot>
+      </template>
+        <van-image
+          v-if="article.cover.type === 1"
+          width="100"
+          height="100"
+          :src="article.cover.images[0]"
+        />
     </van-cell>
   </div>
 </template>
