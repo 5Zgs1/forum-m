d928f73badff38b58291336fc3532f735b7d38ef
5Zgs1 <3460755469@qq.com>
2023-04-26 18:03:53 +0800

    [新增功能](master): 样式调整

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,32 +1,39 @@
 <template>
   <div >
-    <van-cell title="danyuge" class="article-item">
-      <template v-slot:title >{{ article.title }}
-        <slot class="title" >
-          <div class="cover-wrap" v-if="article.cover.type ===3">
-            <div class="cover-item"
-                 v-for="(img, index) in article.cover.images"
-            :key="index">
-              <van-image
-                width="100"
-                height="100"
-                :src="img"
-              />
-            </div>
-          </div>
-          <div>
-            <span>{{article.aut_name}}</span>
-            <span>{{article.comm_count}}条评论</span>
-            <span>{{article.pubdate}}</span>
-          </div>
-        </slot>
+    <van-cell  class="article-item">
+      <template v-slot:title>
+        <div class="van-multi-ellipsis--l2" >
+          {{ article.title }}
+        </div>
       </template>
-        <van-image
-          v-if="article.cover.type === 1"
-          width="100"
-          height="100"
-          :src="article.cover.images[0]"
-        />
+      <template v-slot:label>
+        <div class="cover-wrap" v-if="article.cover.type ===3">
+          <div class="cover-item"
+               v-for="(img, index) in article.cover.images"
+               :key="index">
+            <van-image
+              class="cover-item-img"
+              fit="cover"
+              :src="img"
+            />
+          </div>
+        </div>
+        <div class="label-info-wrap">
+          <span>{{article.aut_name}}</span>
+          <span>{{article.comm_count}}评论</span>
+          <span>{{article.pubdate}}</span>
+        </div>
+      </template>
+      <!--fit 设置为不拉伸-->
+      <van-image
+        v-if="article.cover.type === 1"
+        v-slot: default
+        class="right-cover"
+        fit="cover"
+        width="100"
+        height="100"
+        :src="article.cover.images[0]"
+      />
     </van-cell>
   </div>
 </template>
@@ -51,5 +58,41 @@ export default {
 </script>
 
 <style scoped lang="less">
+
+.article-item {
+  // 不参与页面占用
+  // TODO:不起作用
+  .van-cell__value {
+    flex: unset;
+    width: 232px;
+    height: 146px;
+    padding-left: 25px;
+  }
+  .title {
+    font-size: 32px;
+    color: #3a3a3a;
+  }
+  .right-cover {
+    width: 232px;
+    height: 146px;
+  }
+  .label-info-wrap span {
+    font-size: 22px;
+    color: #b4b4b4;
+    margin-right: 25px;
+  }
+  .cover-wrap {
+    display: flex;
+    padding: 30px 0;
+    .cover-item {
+      flex: 1;
+      height: 146px;
+      .cover-item-img {
+        width: 100%;
+        height: 146px;
+      }
+    }
+  }
+}
 
 </style>
